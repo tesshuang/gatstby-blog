3b8328f808fb388fd41967648a1b53b702629e76
Tess <tesshuangxj@yahoo.com>
2020-01-02 12:17:09 -0800

    Add about page and contact page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,17 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi Gatsby</h1>
+    <p>What a world.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div >
+      <Link style={{ margin: `5px` }} to="/about">About</Link>
+      <Link style={{ margin: `5px` }} to="/contact/">Contact</Link>
+    </div>
+
   </Layout>
 )
 
