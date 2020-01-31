67572103f2041a32390e90ad11cb3f9c3b0ec998
Tess <tesshuangxj@yahoo.com>
2020-01-30 21:16:55 -0800

    Add formik component to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Form from "../components/form"
 
 export const query = graphql`
   query {
@@ -18,6 +19,7 @@ const Contact = ({data}) => (
   <Layout>
     <SEO title="Contact" />
     <h1>Contact me</h1>
+    <Form />
     <p>Data from query: {data.site.siteMetadata.title}</p>
     <p>Let's work together!</p>
   </Layout>
