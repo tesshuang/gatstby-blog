069c9f70190ada6a9664345e305baa75e65f7b66
Tess <tesshuangxj@yahoo.com>
2020-02-09 20:40:42 -0800

    Added textarea to NetForm to see if it could fixed the rendering problem in the hosting site

--- src/components/netlifyForm.js
+++ src/components/netlifyForm.js
@@ -7,8 +7,9 @@ const NetiflyForm = () => {
       <h3>Stay In Touch</h3>
       <p>Subscribe to my mailing list to get more tips about learning Gatsby</p>
       <form name="subscribe" method="POST" data-netlify="true" action="/success" className={styles.netForm}>
-        <input type="text" name="name" placeholder="Your name" className={styles.input}/>
-        <input type="email" name="email" placeholder="Your email" className={styles.input}/>
+        <input type="text" name="name" placeholder="Your name" className={styles.input} required/>
+        <input type="email" name="email" placeholder="Your email" className={styles.input} required/>
+        <textarea type="text" name="message" placeholder="Your message" className={styles.input} />
         <input type="submit" className="submit-button" value="Send" />
       </form>
       </div>
