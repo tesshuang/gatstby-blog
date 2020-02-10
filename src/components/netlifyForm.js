de4841c683062ca3767d321c7d22d1ef12ecc800
Tess <tesshuangxj@yahoo.com>
2020-02-09 18:18:36 -0800

    Updated form name to subscribe

--- src/components/netlifyForm.js
+++ src/components/netlifyForm.js
@@ -6,7 +6,7 @@ const NetiflyForm = () => {
     <div className={styles.wrapper}>
       <h3>Stay In Touch</h3>
       <p>Subscribe to my mailing list to get more tips about learning Gatsby</p>
-      <form name="contact" method="POST" data-netlify="true" action="/success" className={styles.netForm}>
+      <form name="subscribe" method="POST" data-netlify="true" action="/success" className={styles.netForm}>
         <input type="text" name="name" placeholder="Your name" className={styles.input}/>
         <input type="email" name="email" placeholder="Your email" className={styles.input}/>
         <input type="submit" className="submit-button" value="Send" />
