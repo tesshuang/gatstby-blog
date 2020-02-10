6d8f198da37c2441741bb645fd948220ef5d73be
Tess <tesshuangxj@yahoo.com>
2020-02-09 21:16:03 -0800

    Manually added a hidden input value

--- src/components/netlifyForm.js
+++ src/components/netlifyForm.js
@@ -7,6 +7,7 @@ const NetiflyForm = () => {
       <h3>Stay In Touch</h3>
       <p>Subscribe to my mailing list to get more tips about learning Gatsby</p>
       <form name="subscribe" method="POST" data-netlify="true" action="/success" className={styles.netForm}>
+        <input type="hidden" name="form-name" value="subscribe" />
         <input type="text" name="name" placeholder="Your name" className={styles.input} required/>
         <input type="email" name="email" placeholder="Your email" className={styles.input} required/>
         <button type="submit" className="submit-button">Send</button>
