74502e2475268f8b1a0360a278cb868fd21d673d
Tess <tesshuangxj@yahoo.com>
2020-02-09 16:46:06 -0800

    Added a Success page for form submission

---
+++ src/pages/success.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const Success = () => {
+  return ( 
+    <Layout>
+      <h3>Thank you for subscribing!</h3>
+    </Layout>
+   );
+}
+ 
+export default Success;
